3d5c828db7e51a613a69e854bdc925576024abc2
Athitheya Gobinathan <athitheya.gobinathan@infotechinc.com>
2023-06-13 10:22:53 -0400

    Added routing for clubhouses, models, and posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "clubhouses#index"
+
+  resources :clubhouses do 
+    resources :posts do
+      resources :comments
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
