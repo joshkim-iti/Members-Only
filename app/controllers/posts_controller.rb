4fc7174780e0f96c8e20b2b16816081c3cdd1ecd
joshkim-iti <josh.kim@infotechinc.com>
2023-06-15 15:15:01 -0400

    reworked post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,7 @@
 class PostsController < ApplicationController
+    def show
+        @clubhouse = Clubhouse.find(params[:clubhouse_id])
+        @user = User.find(params[:user_id])
+        @post = Post.find(params[:id])
+    end
 end
