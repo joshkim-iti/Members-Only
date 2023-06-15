7d8333f9a52681eb05a2092dab2428d62b0407c6
Athitheya Gobinathan <athitheya.gobinathan@infotechinc.com>
2023-06-15 16:15:34 -0400

    Add post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,30 @@ class PostsController < ApplicationController
         @user = User.find(params[:user_id])
         @post = Post.find(params[:id])
     end
+    def new
+        @post = Post.new
+        @clubhouse = Clubhouse.find(params[:clubhouse_id])
+        @user = User.find(params[:user_id])
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @clubhouse = Clubhouse.find(params[:clubhouse_id])
+        @user = User.find(current_user.id)
+
+        if @post.save
+            puts "Saved"
+            redirect_to user_clubhouse_path(user_id: current_user.id, id: @clubhouse.id)
+        else
+            puts "not saved"
+            puts :unprocessable_entity
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body, :user_id, :clubhouse_id)
+    end
 end
