8134ec7cedcb0469bee356a23a48a34b7394e75c
Athitheya Gobinathan <athitheya.gobinathan@infotechinc.com>
2023-06-16 11:23:11 -0400

    Added comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(comment_params)
+        redirect_to clubhouse_post_path(clubhouse_id: params[:comment][:clubhouse_id], id: params[:post_id])
+      end
+    
+      private
+        def comment_params
+          params.require(:comment).permit(:body, :user_id, :post_id)
+        end
 end
